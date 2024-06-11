911307ef9deaa0774e15680ac1d58d35d886a3a6
kaickvargas <kaickvargas2004@gmail.com>
2024-06-11 11:02:40 -0300

    Opção de saída
    Adicionado a opção de saída do sistema para o cliente

--- cartorioebac.cpp
+++ cartorioebac.cpp
@@ -109,6 +109,7 @@ int main()
 	    printf("\t1 - Registrar nomes\n");
 	    printf("\t2 - consultar nomes\n");
 	    printf("\t3 - deletar nomes\n");  
+	    printf("\t4 - sair do sistema\n\n");
         printf("opção: "); //fim do menu
         
         scanf("%d", &opcao); //armazenando  escolha do usuário
@@ -129,6 +130,11 @@ int main()
 	    case 3:
 	    deletar();
         break;
+        
+        case 4 :
+        	printf("obrigado por utilizar o sistema!\n");
+        	return 0;
+        	break;
         
         default:printf("Essa opção não está disponível!\n");
 	    system("pause");
